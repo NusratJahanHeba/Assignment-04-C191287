1492e125d8f113247fc2dad1fb434da5044273c2
NusratJahanHeba <nusratjahanheba1@gmail.com>
2024-01-05 13:43:17 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import nusrat from "underscore";
+
+
+
+const languages=["Java", "Javascript", "Typescript"];
+
+const java_isExist=jaima.contains(languages,"Java");
+const cpp_isExist=jaima.contains(languages,"C++");
+
+
+console.log("Java exists in this Array",java_isExist);
+console.log("C++ exists in this Array",cpp_isExist);
+
